1e5ab02d3401dca8d2d12d45c507f0d4a34a9564
Marisa Saunders <marisaksaunders@gmail.com>
2024-03-26 23:02:28 -0400

    mobile menu

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BiSearch } from 'react-icons/bi'
 import { BsPerson } from 'react-icons/bs'
 import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+import { FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 import './NavbarStyles.css';
 
@@ -24,6 +25,29 @@ function Navbar() {
       </div>
       <div className='hamburger'>
         <HiOutlineMenuAlt4 className='icon'/>
+      </div>
+      
+      <div className='mobile-menu'>
+        <ul className='mobile-nav'>
+        <li>Home</li>
+        <li>Destinations</li>
+        <li>Travel</li>
+        <li>Book</li>
+        <li>Views</li> 
+        </ul>
+        <div className='mobile-menu-bottom'>
+          <div className="menu-icons">
+            <button>Search</button>
+            <button>Account</button>
+          </div>
+          <div className="social-icons">
+            <FaFacebook class="icon"/>
+            <FaInstagram class="icon"/>
+            <FaTwitter class="icon"/>
+            <FaPinterest class="icon"/>
+            <FaYoutube class="icon"/>
+          </div>
+        </div>
       </div>
     </div>
   )
